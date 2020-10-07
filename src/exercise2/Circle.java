9391ca44742b7c92db083b36c04cc03f292582c8
Aleksandar699 <aleksandar.mihaylov@fdiba.tu-sofia.bg>
2020-10-07 15:30:54 +0000

    ?

---
+++ src/exercise2/Circle.java
@@ -0,0 +1,21 @@
+package exercise2;
+import java.util.Scanner;
+import java.lang.Math;
+public class Circle{
+
+  public static void main(String[] args){
+Scanner input=new Scanner (System.in);
+int r=input.nextInt();
+double C=2*Math.PI*r;
+double A=Math.PI*r*r;
+System.out.println("Circumference=" +C);
+System.out.println("Area=" +A);
+  }
+
+}
+
+
+
+
+
+  
