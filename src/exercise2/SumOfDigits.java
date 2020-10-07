0e212cd03b6d43c976e60a36d17a2971d8d78e18
Aleksandar699 <aleksandar.mihaylov@fdiba.tu-sofia.bg>
2020-10-07 16:06:20 +0000

    ?

---
+++ src/exercise2/SumOfDigits.java
@@ -0,0 +1,16 @@
+package exercise2;
+import java.util.Scanner; 
+
+public class SumOfDigits{
+public static void main(String[] args) {
+  Scanner input=new Scanner(System.in);
+  System.out.print("Enter a number ");
+  int number=input.nextInt();
+  int a=number % 10;
+  number=number / 10;
+  int b= number % 10;
+  number=number / 10;
+  int c=number % 10;
+  System.out.println("Sum of digits: " + (a + b + c) );
+}
+}
